7dff8b6aaa83d124df6986489cd53ae794367ae5
Yiorgis Gozadinos <ggozadinos@gmail.com>
2023-10-17 23:02:17 +0200

    Remove pydantic and as a result the rust dependency.

--- oterm/config.py
+++ oterm/config.py
@@ -1,15 +1,65 @@
+import json
 import os
+from typing import Union, get_type_hints
 
 from dotenv import load_dotenv
-from pydantic import BaseModel
 
 load_dotenv()
 
 
-class AppConfig(BaseModel):
+class AppConfigError(Exception):
+    pass
+
+
+def _parse_bool(val: Union[str, bool]) -> bool:
+    return val if isinstance(val, bool) else val.lower() in ["true", "yes", "1"]
+
+
+class AppConfig:
+    """
+    Map environment variables to class fields according to these rules:
+      - Field won't be parsed unless it has a type annotation
+      - Field will be skipped if not in all caps
+    """
+
     ENV: str = "development"
     OLLAMA_URL: str = "http://localhost:11434/api"
 
+    def __init__(self, env):
+        for field in self.__annotations__:
+            if not field.isupper():
+                continue
+
+            # Raise AppConfigError if required field not supplied
+            default_value = getattr(self, field, None)
+            if default_value is None and env.get(field) is None:
+                raise AppConfigError("The {} field is required".format(field))
+
+            # Cast env var value to expected type and raise AppConfigError on failure
+            try:
+                var_type = get_type_hints(AppConfig)[field]
+                if var_type == bool:
+                    value = _parse_bool(env.get(field, default_value))
+                elif var_type == list[str]:
+                    value = env.get(field)
+                    if value is None:
+                        value = default_value
+                    else:
+                        value = json.loads(value)
+                else:
+                    print(env.get(field))
+                    value = var_type(env.get(field, default_value))
+                self.__setattr__(field, value)
+            except ValueError:
+                raise AppConfigError(
+                    'Unable to cast value of "{}" to type "{}" for "{}" field'.format(
+                        env[field], var_type, field  # type: ignore
+                    )
+                )
+
+    def __repr__(self):
+        return str(self.__dict__)
+
 
 # Expose Config object for app to import
-Config = AppConfig.model_validate(os.environ)
+Config = AppConfig(os.environ)
