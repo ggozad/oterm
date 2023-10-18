40e55ecaad41a90e5dfbbb2ab0bbf97dd32c15b5
Yiorgis Gozadinos <ggozadinos@gmail.com>
2023-10-18 11:14:12 +0200

    Show model info on model selection

--- oterm/app/model_selection.py
+++ oterm/app/model_selection.py
@@ -1,11 +1,13 @@
+from ast import literal_eval
+
 from rich.text import Text
 from textual.app import ComposeResult
-from textual.containers import Container, Horizontal
+from textual.containers import Container, Horizontal, VerticalScroll
 from textual.css.query import NoMatches
 from textual.reactive import reactive
 from textual.screen import ModalScreen
 from textual.widget import Widget
-from textual.widgets import Label, OptionList
+from textual.widgets import Label, OptionList, Pretty, TextArea
 
 from oterm.ollama import OllamaAPI
 
@@ -14,41 +16,77 @@ class ModelDetails(Widget):
     name: reactive[str] = reactive("")
     tag: reactive[str] = reactive("")
     bytes: reactive[int] = reactive(0)
+    model_info: reactive[dict[str, str]] = reactive({}, layout=True)
+    template: reactive[str] = reactive("")
+    system: reactive[str] = reactive("")
+    params: reactive[list[tuple[str, str]]] = reactive([], layout=True)
 
     def watch_name(self, name: str) -> None:
         try:
             widget = self.query_one(".name", Label)
-            if widget:
-                widget.update(f"Name: {self.name}")
+            widget.update(f"Name: {self.name}")
         except NoMatches:
             pass
 
     def watch_tag(self, tag: str) -> None:
         try:
             widget = self.query_one(".tag", Label)
-            if widget:
-                widget.update(f"Tag: {self.tag}")
+            widget.update(f"Tag: {self.tag}")
         except NoMatches:
             pass
 
     def watch_bytes(self, size: int) -> None:
         try:
             widget = self.query_one(".size", Label)
-            if widget:
-                widget.update(f"Size: {(self.bytes / 1.0e9):.2f} GB")
+            widget.update(f"Size: {(self.bytes / 1.0e9):.2f} GB")
+        except NoMatches:
+            pass
+
+    def watch_model_info(self, model_info: dict[str, str]) -> None:
+        self.template = model_info.get("template", "")
+        self.system = model_info.get("system", "")
+        params = model_info.get("parameters", "")
+        lines = params.split("\n")
+        params = []
+        for line in lines:
+            if line:
+                key, value = line.split()
+                try:
+                    value = literal_eval(value)
+                except (SyntaxError, ValueError):
+                    pass
+                params.append((key, value))
+        self.params = params
+
+        try:
+            widget = self.query_one(".parameters", Pretty)
+            widget.update(self.params)
+            widget = self.query_one(".template", TextArea)
+            widget.clear()
+            widget.load_text(self.template)
+            widget = self.query_one(".system", TextArea)
+            widget.load_text(self.system)
         except NoMatches:
             pass
 
     def compose(self) -> ComposeResult:
-        yield Label("Model details:", classes="title")
-        yield Label(f"Name: {self.name}", classes="name")
-        yield Label(f"Tag: {self.tag}", classes="tag")
-        yield Label(f"Size: {self.bytes}", classes="size")
+        with VerticalScroll():
+            yield Label("Model info:", classes="title")
+            yield Label("", classes="name")
+            yield Label("", classes="tag")
+            yield Label("", classes="size")
+            yield Label("Template:", classes="title")
+            yield TextArea(classes="template log")
+            yield Label("System:", classes="title")
+            yield TextArea("", classes="system log")
+            yield Label("Parameters:", classes="title")
+            yield Pretty("", classes="parameters")
 
 
 class ModelSelection(ModalScreen[str]):
     api = OllamaAPI()
     models = []
+    model_info: dict[str, dict] = {}
 
     BINDINGS = [
         ("escape", "cancel", "Cancel"),
@@ -60,6 +98,12 @@ class ModelSelection(ModalScreen[str]):
     async def on_mount(self) -> None:
         self.models = await self.api.get_models()
         models = [model["name"] for model in self.models]
+        for model in models:
+            info = await self.api.get_model_info(model)
+            for key in ["modelfile", "license"]:
+                if key in info.keys():
+                    del info[key]
+            self.model_info[model] = info
         option_list = self.query_one("#model-select", OptionList)
         option_list.clear_options()
         for model in models:
@@ -73,13 +117,14 @@ class ModelSelection(ModalScreen[str]):
         self, option: OptionList.OptionHighlighted
     ) -> None:
         model = option.option.prompt
-        model_info = next((m for m in self.models if m["name"] == str(model)), None)
+        model_meta = next((m for m in self.models if m["name"] == str(model)), None)
         model_details = self.query_one("#model-details", ModelDetails)
-        if model_info:
-            name, tag = model_info["name"].split(":")
+        if model_meta:
+            name, tag = model_meta["name"].split(":")
             model_details.name = name
             model_details.tag = tag
-            model_details.bytes = model_info["size"]
+            model_details.bytes = model_meta["size"]
+            model_details.model_info = self.model_info[model_meta["name"]]
 
     @staticmethod
     def model_option(model: str) -> Text:
