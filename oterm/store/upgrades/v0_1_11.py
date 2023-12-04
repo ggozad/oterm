72436851eab4db8f375e0252244dac30b74d3c48
Yiorgis Gozadinos <ggozadinos@gmail.com>
2023-12-04 15:16:29 +0100

    Redo the upgrade of 0.1.11. Bump version again

--- oterm/store/upgrades/v0_1_11.py
+++ oterm/store/upgrades/v0_1_11.py
@@ -6,13 +6,17 @@ import aiosqlite
 
 async def add_format_to_chat(db_path: Path) -> None:
     async with aiosqlite.connect(db_path) as connection:
-        await connection.executescript(
-            """
-            ALTER TABLE chat ADD COLUMN format TEXT;
-            """
-        )
+        try:
+            await connection.executescript(
+                """
+                ALTER TABLE chat ADD COLUMN format TEXT;
+                """
+            )
+        except aiosqlite.OperationalError:
+            pass
 
 
 upgrades: list[tuple[str, list[Callable[[Path], Awaitable[None]]]]] = [
-    ("0.1.11", [add_format_to_chat])
+    ("0.1.11", [add_format_to_chat]),
+    ("0.1.13", [add_format_to_chat]),
 ]
