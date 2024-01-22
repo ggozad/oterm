57115d406ff9a1a3bce08e07280892c5d6ca989c
Yiorgis Gozadinos <ggozadinos@gmail.com>
2024-01-22 16:42:33 +0100

    Allow changing the root of the DirectoryTree in ImageSelect.
    Closes #50

--- oterm/app/image_browser.py
+++ oterm/app/image_browser.py
@@ -4,10 +4,11 @@ from pathlib import Path
 
 from PIL import Image as PILImage
 from PIL import UnidentifiedImageError
+from textual import on
 from textual.app import ComposeResult
 from textual.containers import Container, Horizontal, Vertical
 from textual.screen import ModalScreen
-from textual.widgets import DirectoryTree, Label
+from textual.widgets import DirectoryTree, Input, Label
 
 from oterm.app.widgets.image import IMAGE_EXTENSIONS, Image, ImageDirectoryTree
 
@@ -23,10 +24,10 @@ class ImageSelect(ModalScreen[tuple[Path, str]]):
     async def on_mount(self) -> None:
         dt = self.query_one(ImageDirectoryTree)
         dt.show_guides = False
+        dt.focus()
 
-    async def on_directory_tree_file_selected(
-        self, ev: DirectoryTree.FileSelected
-    ) -> None:
+    @on(DirectoryTree.FileSelected)
+    async def on_image_selected(self, ev: DirectoryTree.FileSelected) -> None:
         try:
             buffer = BytesIO()
             image = PILImage.open(ev.path)
@@ -38,17 +39,28 @@ class ImageSelect(ModalScreen[tuple[Path, str]]):
         except UnidentifiedImageError:
             self.dismiss()
 
-    async def on_tree_node_highlighted(self, ev: DirectoryTree.NodeHighlighted) -> None:
+    @on(DirectoryTree.NodeHighlighted)
+    async def on_image_highlighted(self, ev: DirectoryTree.NodeHighlighted) -> None:
         path = ev.node.data.path
         if path.suffix in IMAGE_EXTENSIONS:
             image = self.query_one(Image)
             image.path = path.as_posix()
+
+    @on(Input.Changed)
+    async def on_root_changed(self, ev: Input.Changed) -> None:
+        dt = self.query_one(ImageDirectoryTree)
+        path = Path(ev.value)
+        if not path.exists() or not path.is_dir():
+            return
+        dt.path = path
 
     def compose(self) -> ComposeResult:
         with Container(id="image-select-container"):
             with Horizontal():
                 with Vertical(id="image-directory-tree"):
                     yield Label("Select an image:", classes="title")
+                    yield Label("Root:")
+                    yield Input(Path("./").resolve().as_posix())
                     yield ImageDirectoryTree("./")
                 with Container(id="image-preview"):
                     yield Image(id="image")
