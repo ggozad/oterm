409495bfda0aa692d74c288c2e480aaa9438e6d6
Abdürrahim YILMAZ <84680978+yilmaz08@users.noreply.github.com>
2024-08-12 18:34:39 +0300

    Merge 2 functions into 1
    Use parameter "change" instead of seperating 2 binding functions

--- oterm/app/oterm.py
+++ oterm/app/oterm.py
@@ -17,8 +17,8 @@ class OTerm(App):
     CSS_PATH = "oterm.tcss"
     BINDINGS = [
         ("ctrl+n", "new_chat", "new"),
-        ("ctrl+tab", "next_chat", "next chat"),
-        ("ctrl+shift+tab", "prev_chat", "prev chat"),
+        ("ctrl+tab", "cycle_chat(+1)", "next chat"),
+        ("ctrl+shift+tab", "cycle_chat(-1)", "prev chat"),
         ("ctrl+t", "toggle_dark", "toggle theme"),
         ("ctrl+q", "quit", "quit"),
     ]
@@ -30,7 +30,7 @@ class OTerm(App):
     async def action_quit(self) -> None:
         return self.exit()
 
-    async def action_next_chat(self) -> None:
+    async def action_cycle_chat(self, change: int) -> None:
         tabs = self.query_one(TabbedContent)
         saved_chats = await self.store.get_chats()
         if tabs.active_pane is None:
@@ -38,22 +38,9 @@ class OTerm(App):
         active_id = int(str(tabs.active_pane.id).split("-")[1])
         for _chat in saved_chats:
             if _chat[0] == active_id:
-                next_index = (saved_chats.index(_chat) + 1) % len(saved_chats)
+                next_index = (saved_chats.index(_chat) + change) % len(saved_chats)
                 next_id = saved_chats[next_index][0]
                 tabs.active = f"chat-{next_id}"
-                break
-        
-    async def action_prev_chat(self) -> None:
-        tabs = self.query_one(TabbedContent)
-        saved_chats = await self.store.get_chats()
-        if tabs.active_pane is None:
-            return
-        active_id = int(str(tabs.active_pane.id).split("-")[1])
-        for _chat in saved_chats:
-            if _chat[0] == active_id:
-                prev_index = (saved_chats.index(_chat) - 1) % len(saved_chats)
-                prev_id = saved_chats[prev_index][0]
-                tabs.active = f"chat-{prev_id}"
                 break
 
     def action_new_chat(self) -> None:
