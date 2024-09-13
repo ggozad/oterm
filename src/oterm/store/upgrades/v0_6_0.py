93d5ee20b6b28e461af53e44f7a81a1867fe2dc6
Yiorgis Gozadinos <ggozadinos@gmail.com>
2024-09-13 14:40:41 +0200

    DB migration to store tools in chat table

---
+++ src/oterm/store/upgrades/v0_6_0.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from typing import Awaitable, Callable
+
+import aiosqlite
+
+
+async def tools(db_path: Path) -> None:
+    async with aiosqlite.connect(db_path) as connection:
+        try:
+            await connection.executescript(
+                """
+                ALTER TABLE chat ADD COLUMN tools TEXT DEFAULT "[]";
+                """
+            )
+        except aiosqlite.OperationalError:
+            pass
+
+        await connection.commit()
+
+
+upgrades: list[tuple[str, list[Callable[[Path], Awaitable[None]]]]] = [
+    ("0.6.0", [tools])
+]
