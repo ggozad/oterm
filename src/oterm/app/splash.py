74124bc35abe90080a2b13a58b4649046d9623c3
Yiorgis Gozadinos <ggozadinos@gmail.com>
2024-09-25 07:40:44 +0200

    Pick random effect from effect gallery

--- src/oterm/app/splash.py
+++ src/oterm/app/splash.py
@@ -1,4 +1,9 @@
+import random
+from typing import Any, Tuple
+
 from textualeffects.widgets import SplashScreen
+
+from oterm.app.widgets.effect import EffectType
 
 logo = """
          @@@@@@.                      :@@@@@@
@@ -42,12 +47,53 @@ logo = """
     @.                                          .@
 """
 
+effects: list[Tuple[EffectType, dict[str, Any]]] = [
+    (
+        "Beams",
+        {
+            "beam_delay": 3,
+            "beam_gradient_steps": 2,
+            "beam_gradient_frames": 2,
+            "final_gradient_steps": 2,
+            "final_gradient_frames": 2,
+            "final_wipe_speed": 5,
+        },
+    ),
+    (
+        "BouncyBalls",
+        {
+            "ball_delay": 1,
+        },
+    ),
+    (
+        "Expand",
+        {
+            "movement_speed": 0.1,
+        },
+    ),
+    (
+        "Pour",
+        {
+            "pour_speed": 3,
+        },
+    ),
+    (
+        "Rain",
+        {},
+    ),
+    (
+        "RandomSequence",
+        {},
+    ),
+    (
+        "Scattered",
+        {},
+    ),
+    (
+        "Slide",
+        {},
+    ),
+]
 
-splash = SplashScreen(
-    text=logo,
-    effect="Spotlights",
-    config={
-        "search_duration": 100,
-        "spotlight_count": 3,
-    },
-)
+effect = random.choice(effects)
+splash = SplashScreen(text=logo, effect=effect[0], config=effect[1])
