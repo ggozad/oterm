deccefc8c0ecab1437079acc2e1df3caaa47386f
Yiorgis Gozadinos <ggozadinos@gmail.com>
2024-09-27 12:28:18 +0200

    Support for loading custom tools.

--- src/oterm/tools/__init__.py
+++ src/oterm/tools/__init__.py
@@ -1,11 +1,43 @@
-from typing import Sequence
+from importlib import import_module
+from typing import Awaitable, Callable, Sequence
+
+from ollama._types import Tool
 
 from oterm.tools.date_time import DateTimeTool, date_time
 from oterm.tools.location import LocationTool, current_location
 from oterm.tools.shell import ShellTool, shell_command
 from oterm.tools.weather import WeatherTool, current_weather
 from oterm.tools.web import WebTool, fetch_url
-from oterm.types import ToolDefinition
+from oterm.types import ExternalToolDefinition, ToolDefinition
+
+
+def load_tools(tool_defs: Sequence[ExternalToolDefinition]) -> Sequence[ToolDefinition]:
+    tools = []
+    for tool_def in tool_defs:
+        tool_path = tool_def["tool"]
+
+        try:
+            module, tool = tool_path.split(":")
+            module = import_module(module)
+            tool = getattr(module, tool)
+            if not isinstance(tool, Tool):
+                raise Exception(f"Expected Tool, got {type(tool)}")
+        except ModuleNotFoundError as e:
+            raise Exception(f"Error loading tool {tool_path}: {str(e)}")
+
+        callable_path = tool_def["callable"]
+        try:
+            module, function = callable_path.split(":")
+            module = import_module(module)
+            callable = getattr(module, function)
+            if not isinstance(callable, (Callable, Awaitable)):
+                raise Exception(f"Expected Callable, got {type(callable)}")
+        except ModuleNotFoundError as e:
+            raise Exception(f"Error loading callable {callable_path}: {str(e)}")
+        tools.append({"tool": tool, "callable": callable})
+
+    return tools
+
 
 available: Sequence[ToolDefinition] = [
     {"tool": DateTimeTool, "callable": date_time},
---
+++ tests/tools/test_custom_tool.py
@@ -0,0 +1,22 @@
+import pytest
+
+from oterm.tools import load_tools
+from oterm.tools.date_time import DateTimeTool, date_time
+
+
+@pytest.mark.asyncio
+async def test_loading_custom_tool():
+
+    # Test loading a callable from a well-defined module
+    tools = load_tools(
+        [
+            {
+                "tool": "oterm.tools.date_time:DateTimeTool",
+                "callable": "oterm.tools.date_time:date_time",
+            }
+        ]
+    )
+
+    assert len(tools) == 1
+    assert tools[0]["tool"] == DateTimeTool
+    assert tools[0]["callable"] == date_time
