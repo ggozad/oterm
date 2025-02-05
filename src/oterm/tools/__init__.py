e43a7ae9640c65b5e93f4e9a6801f441842753ff
Thomas Sheffler <tom.sheffler@gmail.com>
2025-02-05 13:05:29 -0800

    Two things: make MCP servers long-running and cleanly terminate upon exit. Also add logging so messages go to file instead of screen.

--- src/oterm/tools/__init__.py
+++ src/oterm/tools/__init__.py
@@ -50,24 +50,31 @@ external_tools = appConfig.get("tools")
 if external_tools:
     available.extend(load_tools(external_tools))
 
+client_list = [ ]
 
 async def setup_mcp_servers():
     mcp_servers = appConfig.get("mcpServers")
     tool_defs: list[ToolDefinition] = []
     if mcp_servers:
         for server, config in mcp_servers.items():
+            # OPEN A LOG FOR THIS SERVER using the same naming Claude Desktop does
+            errlog = open(f"mcp-server-{server}.log", "w")
+            print(f"==== Starting {server} ====\n", file=errlog)
+            errlog.flush()
             async with MCPClient(
-                StdioServerParameters.model_validate(config)
+                StdioServerParameters.model_validate(config), errlog=errlog
             ) as client:
                 mcp_tools: list[MCPTool] = await client.get_available_tools()
+                client_list.append(client)
 
                 for mcp_tool in mcp_tools:
                     tool = mcp_tool_to_ollama_tool(mcp_tool)
 
                     async def callable(name=mcp_tool.name, **kwargs):
-                        async with MCPClient(
-                            StdioServerParameters.model_validate(config)
-                        ) as cl:
+                        async with client as cl:
+                            # TO REMOVE THESE TWO LINES
+                            print(f"==== Call Tool {name} ====\n", file=errlog)
+                            errlog.flush()
                             res: CallToolResult = await cl.call_tool(name, kwargs)
                             if res.isError:
                                 raise Exception(f"Error call mcp tool {name}.")
@@ -79,3 +86,9 @@ async def setup_mcp_servers():
                     tool_defs.append({"tool": tool, "callable": callable})
 
     return tool_defs
+
+
+async def teardown_mcp_servers():
+    for client in client_list:
+        await client.session.__aexit__(None, None, None)
+        await client._client.__aexit__(None, None, None)
