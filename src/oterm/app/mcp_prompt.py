190f50302af45d7ae9e86e9e0e427060569b9d2f
Yiorgis Gozadinos <ggozadinos@gmail.com>
2025-03-18 17:52:39 +0100

    PromptFormWidget displaying basic prompt editing.

--- src/oterm/app/mcp_prompt.py
+++ src/oterm/app/mcp_prompt.py
@@ -1,4 +1,5 @@
 from mcp.types import Prompt
+from textual import on
 from textual.app import ComposeResult, RenderResult
 from textual.containers import (
     Container,
@@ -7,19 +8,38 @@ from textual.containers import (
 )
 from textual.screen import ModalScreen
 from textual.widget import Widget
-from textual.widgets import Button, Label, OptionList
+from textual.widgets import Button, Label, Markdown, OptionList, TextArea
 from textual.widgets.option_list import Option
 
 from oterm.tools.mcp import mcp_prompts
 
 
-class PromptWidget(Widget):
+class PromptOptionWidget(Widget):
     def __init__(self, prompt: Prompt) -> None:
         super().__init__()
         self.prompt = prompt
 
     def render(self) -> RenderResult:
         return f"[b]{self.prompt.name}[/b]\n[i]{self.prompt.description}[/i]"
+
+
+class PromptFormWidget(Widget):
+    prompt: Prompt
+
+    @on(TextArea.Changed)
+    async def on_text_area_change(self, ev: TextArea.Changed):
+        arg_name = (ev.text_area.id or "").split("arg-")[1]
+        print(arg_name)
+
+    def compose(self) -> ComposeResult:
+        yield Label(self.prompt.name)
+        yield Label(self.prompt.description or "")
+        with Vertical(id="prompt-form"):
+            for arg in self.prompt.arguments or []:
+                yield Label(arg.name, classes="subtitle")
+                yield Label(arg.description or "", classes="subtitle")
+                yield TextArea(id=f"arg-{arg.name}")
+        yield Markdown(id="prompt-result")
 
 
 class MCPPrompt(ModalScreen[str]):
@@ -42,12 +62,20 @@ class MCPPrompt(ModalScreen[str]):
 
     @staticmethod
     def prompt_option(prompt: Prompt) -> Option:
-        return Option(prompt=PromptWidget(prompt).render(), id=prompt.name)
+        return Option(prompt=PromptOptionWidget(prompt).render(), id=prompt.name)
 
     def on_option_list_option_highlighted(
         self, option: OptionList.OptionHighlighted
     ) -> None:
-        print(option.option.id)
+        for prompt in mcp_prompts:
+            if prompt.name == option.option.id:
+                break
+
+        form_container = self.query_one("#prompt-form", Vertical)
+        form_container.remove_children()
+        widget = PromptFormWidget()
+        widget.prompt = prompt
+        form_container.mount(widget)
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.name == "copy":
@@ -64,7 +92,7 @@ class MCPPrompt(ModalScreen[str]):
 
                 with Vertical():
                     yield Label("Customize prompt:", classes="title")
-
+                    yield Vertical(id="prompt-form")
             with Horizontal(classes="button-container"):
                 yield Button(
                     "Copy",
