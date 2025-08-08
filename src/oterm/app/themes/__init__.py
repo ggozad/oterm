3de5a934d08a7b56b68a8d8db4871ab5767723d7
James <jameskowal10@gmail.com>
2025-08-08 14:15:18 +0100

    Seperate into module

--- src/oterm/app/themes/__init__.py
+++ src/oterm/app/themes/__init__.py
@@ -1,20 +1,3 @@
-from textual.theme import Theme
-
-
-solarized_dark = Theme(
-    name="solarized-dark",
-    primary="#268bd2",
-    secondary="#859900",
-    accent="#6c71c4",
-    foreground="#93a1a1",
-    background="#002b36",
-    success="#859900",
-    warning="#b58900",
-    error="#dc322f",
-    surface="#073642",
-    panel="#073642",
-    dark=True,
-    variables={},
-)
+from . import solarized_dark
 
 __all__ = ["solarized_dark"]
